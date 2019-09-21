aa66687bb0d3f867a07c3cd4b21e37c901571ff7
kamgirgit <kamran.jmi12@gmail.com>
2019-09-22 02:08:17 +0530

    new changes

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{ip-locator}}'));
+app.use(express.static('./dist/{{browser}}'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/{{ip-locator}}/index.html'));
+res.sendFile(path.join(__dirname,'/dist/{{browser}}/index.html'));
 });
 
 // Start the app by listening on the default Heroku port

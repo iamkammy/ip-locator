869394f3099c56c7a394fb1ef1fe2b582ccee8f0
kamgirgit <kamran.jmi12@gmail.com>
2019-09-22 16:58:29 +0530

    new way to handle all routes ,  server.js is at root section

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');  
 const app = express();  
+const port = process.env.PORT || 2332;
 app.use(express.static(__dirname + '/dist/browser/'));  
 app.all('*', (req, res) => {  
   res.status(200).sendFile(__dirname + '/dist/browser/index.html');  
 });  
-app.listen(process.env.PORT || 2332);  
+app.listen(port, ()=>{
+    console.log('server is listening at port'+ port );
+});  

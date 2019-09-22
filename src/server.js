d8b5a730ce3620adab04fd120f442de5260164c8
kamgirgit <kamran.jmi12@gmail.com>
2019-09-22 15:49:24 +0530

    browser added in path

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');  
 const app = express();  
-app.use(express.static(__dirname + '/dist'));  
+app.use(express.static(__dirname + '/dist/browser/'));  
 app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/dist/index.html');  
+  res.status(200).sendFile(__dirname + '/dist/browser/index.html');  
 });  
-app.listen(process.env.PORT || 8080);  
+app.listen(process.env.PORT || 2332);  

529be24b7858804462fce8168f61f88ed3830c17
kamgirgit <kamran.jmi12@gmail.com>
2019-09-22 15:41:37 +0530

    new way of server.js handling

--- src/server.js
+++ src/server.js
@@ -1,16 +1,7 @@
-//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const express = require('express');  
+const app = express();  
+app.use(express.static(__dirname + '/dist'));  
+app.all('*', (req, res) => {  
+  res.status(200).sendFile(__dirname + '/dist/index.html');  
+});  
+app.listen(process.env.PORT || 8080);  
